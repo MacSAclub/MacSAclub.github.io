7e0a32738812df07b84e97527a40d266978c8697
Christopher Lamers <github@lamersc.com>
2023-10-06 20:15:14 -0600

    Revised look

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-import { greetings } from "./greetings.js"
+import { welcome_languages } from "./greetings.js"
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
 function getRandomInt(min, max) {
@@ -7,10 +7,27 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-const welcome_message = document.getElementById("welcome-message")
-setInterval(
-  () => {
-    welcome_message.innerText = greetings[getRandomInt(0, greetings.length)].hello
-  }, 100
-)
-document.getElementById("welcome-message").innerText = "test"
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener("DOMContentLoaded", async (event) => {
+  const slogan_elem = document.getElementById("slogan")
+
+  while(true) {
+    const new_elem = document.createElement("p");
+    const elem_style = new_elem.style;
+    new_elem.innerText = welcome_languages[getRandomInt(0, welcome_languages.length - 1)];
+    elem_style.position = "absolute";
+    elem_style.left = `${getRandomInt(5, 90)}%`;
+    elem_style.top = `${getRandomInt(5, 90)}%`;
+    elem_style.animation = "fadeIn 5s";
+    slogan_elem.appendChild(new_elem);
+    await timeout(50)
+    setTimeout(async () => {
+      elem_style.animation = "fadeOut 3s";
+      await timeout(5000)
+      new_elem.remove()
+    }, 5000)
+  }
+});
